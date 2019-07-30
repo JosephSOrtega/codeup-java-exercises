f13ce6e1f66eef3b5f7c9da193c55ae6b9b0e7fc
JustinSOrtega <justinsortega@gmail.com>
2019-07-30 13:43:16 -0500

    ex 2 done OOP

--- src/Person.java
+++ src/Person.java
@@ -1,13 +1,25 @@
 public class Person {
     private static String name;
 
+
+    private Person(String here) {
+        this.name = here;
+    }
+
+
     // returns the person's name
-    private static void getName() {
-        System.out.println(name);
+//    private static void getName() {
+//        System.out.println(name);
+//    }
+    private static String getName() {
+        return Person.name;
     }
 
     // changes the name property to the passed value
-    private Person(String nameHere) {
+//    private Person(String nameHere) {
+//        this.name = nameHere;
+//    }
+    private void setName(String nameHere) {
         this.name = nameHere;
     }
 
@@ -21,6 +33,23 @@ public class Person {
         System.out.println("Their name is "+ Dave.name);
         getName();
         sayHello();
+
+        Person person1 = new Person("John");
+        Person person2 = new Person("John");
+        System.out.println(person1.getName().equals(person2.getName()));
+        System.out.println(person1 == person2);
+
+        person1 = new Person("John");
+        person2 = person1;
+        System.out.println(person1 == person2);
+
+        person1 = new Person("John");
+        person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
     }
 
 
