af55298143b4efbdcd383a5fbf57a40ec528a776
JustinSOrtega <justinsortega@gmail.com>
2019-07-30 12:33:32 -0500

    ex 1 complete OOP

--- src/Person.java
+++ src/Person.java
@@ -1,29 +1,26 @@
-import java.util.Scanner;
-
-class Person {
-    private String name;
-
-    private void main(String[] args) {
-        Person Dave = new Person("dave");
-        System.out.println("Their name is "+ Dave.name);
-        getName();
-        sayHello();
-    }
-
+public class Person {
+    private static String name;
 
     // returns the person's name
-    public void getName() {
+    private static void getName() {
         System.out.println(name);
     }
 
     // changes the name property to the passed value
-    public Person(String nameHere) {
+    private Person(String nameHere) {
         this.name = nameHere;
     }
 
     // prints a message to the console using the person's name
-    private void sayHello() {
+    private static void sayHello() {
         System.out.println("your name is " + name);
+    }
+
+    public static void main(String[] args) {
+        Person Dave = new Person("dave");
+        System.out.println("Their name is "+ Dave.name);
+        getName();
+        sayHello();
     }
 
 
