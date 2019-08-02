8aa404125912962e57327f6c616d37263651deda
JustinSOrtega <justinsortega@gmail.com>
2019-08-02 14:31:29 -0500

    starting on party roll

---
+++ src/Items.java
@@ -0,0 +1,25 @@
+public class Items {
+    static int gold;
+    static int molotov;
+    static int blade;
+    static int charm;
+    static int shield;
+    static int plate;
+    static int powder;
+    static int potion;
+    static int stone;
+    static int scroll;
+    static int hat;
+    static int bar;
+
+
+//move to character
+    static int xp;
+    static int energy;
+    static int maxEnergy;
+    static int moveLevel;
+    static int bonusForward;
+    static int negForward;
+
+
+}
