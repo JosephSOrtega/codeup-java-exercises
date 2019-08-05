9985e8a535e058896c3dbe0bf8dc8d8192332a02
JustinSOrtega <justinsortega@gmail.com>
2019-08-05 10:43:23 -0500

    finished 3 ex of collections

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,6 +1,7 @@
 package grades;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GradesApplication {
     public static void main(String[] args) {
@@ -29,6 +30,36 @@ public class GradesApplication {
         System.out.println(students.get("JohnDoe").getGradeAverage());
         System.out.println(students.get("JaneDoe").getGradeAverage());
         System.out.println(students.get("DaveDudeman").getGradeAverage());
+
+        System.out.println(students.get("JosephSOrtega"));
+        System.out.println(students.get("JohnDoe"));
+        System.out.println(students.get("JaneDoe"));
+        System.out.println(students.get("DaveDudeman"));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome!\n+" +
+                "Here are the GitHub usernames of our students:\n " +
+                "|JosephSOrtega| |JohnDoe| |JaneDoe| |DaveDudeman|");
+
+            String cont = "y";
+            do {
+                System.out.println("What student would you like to see more information on?");
+                String answer = scanner.nextLine();
+
+                if (students.containsKey(answer)) {
+                    System.out.println("Name: " + students.get(answer) + " - GitHub Username: " + students.get(answer).getName());
+                System.out.println("Current Average: " + students.get(answer).getGradeAverage() + "\n");
+                System.out.println("Would you like to see another student? [y/n]");
+                cont = scanner.nextLine();
+                } else {
+                    System.out.println("Sorry, no student found with the GitHub username of \"" + answer + "\".");
+                }
+
+            } while (cont.equalsIgnoreCase("y"));
+        System.out.println("Goodbye, and have a wonderful day!");
+
+
+
 
 
 
