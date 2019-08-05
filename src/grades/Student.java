fd1402f025ea7dae416acb5c8d8c5394850786e8
JustinSOrtega <justinsortega@gmail.com>
2019-08-05 09:47:30 -0500

    finished 1 ex of collections

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -6,26 +6,49 @@ public class Student {
     String name;
     ArrayList grades;
 
-    public Student(String name, ArrayList grades) {
+    public Student(String name) {
         this.name = name;
-        this.grades = grades;
+        this.grades = new ArrayList<Double>();
     }
+
     // returns the student's name
     public String getName() {
+        System.out.println(this.name);
         return this.name;
     }
+
     // adds the given grade to the grades property
-    public void addGrade(int grade) {
+    public void addGrade(double grade) {
         this.grades.add(grade);
     }
+
     // returns the average of the students grades
     public double getGradeAverage() {
-        this.grades;
+        double total = 0;
+
+        for (Object num : this.grades) {
+            double nums = (double) num;
+            total += nums;
+        }
+        total /= this.grades.size();
+        System.out.println(total);
+        return total;
+
     }
+
+
 
 
     public static void main(String[] args) {
-        ArrayList<String> gradeList = new ArrayList<>();
 
+        Student example = new Student("namesHere");
+        example.getName();
+        example.addGrade(5);
+        example.addGrade(4);
+        example.addGrade(5);
+        example.addGrade(3);
+        example.addGrade(5);
+        example.getGradeAverage();
     }
+
 }
